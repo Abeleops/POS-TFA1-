53b9b2957b0436eb4549d17abc1b90741853633b
Abeleops <abelejohnjuarez03@gmail.com>
2026-09-21 01:20:30 +0800

    Fixed: updated customer and user data in Home controller

--- TFA1/app/Controllers/Home.php
+++ TFA1/app/Controllers/Home.php
@@ -8,4 +8,85 @@ class Home extends BaseController
     {
         return view('pages/home');
     }
+
+    public function about(): string
+    {
+        return view('pages/about');
+    }
+
+    public function customers(): string
+    {
+        return view('customers/index', [
+            'customers' => [
+                [
+                    'full_name' => 'Alex Morgan',
+                    'email' => 'alex@Gmail.com',
+                    'phone' => '555-0101',
+                ],
+                [
+                    'full_name' => 'Jordan Lee',
+                    'email' => 'jordan@Gmail.com',
+                    'phone' => '555-0102',
+                ],
+                  [
+                    'full_name' => 'Alex Morgan',
+                    'email' => 'alex@Gmail.com',
+                    'phone' => '555-0101',
+                ],
+                [
+                    'full_name' => 'Jordan Lee',
+                    'email' => 'jordan@Gmail.com',
+                    'phone' => '555-0102',
+                ],
+                  [
+                    'full_name' => 'Alex Morgan',
+                    'email' => 'alex@Gmail.com',
+                    'phone' => '555-0101',
+                ],
+                [
+                    'full_name' => 'Jordan Lee',
+                    'email' => 'jordan@Gmail.com',
+                    'phone' => '555-0102',
+                ],
+            ],
+        ]);
+    }
+
+    public function users(): string
+    {
+        return view('users/index', [
+            'users' => [
+                [
+                    'username' => 'admin',
+                    'full_name' => 'Admin User',
+                    'role' => 'Administrator',
+                ],
+                [
+                    'username' => 'manager1',
+                    'full_name' => 'Store Manager',
+                    'role' => 'Manager',
+                ],
+                  [
+                    'username' => 'admin1',
+                    'full_name' => 'Admin User',
+                    'role' => 'Administrator',
+                ],
+                [
+                    'username' => 'manager2',
+                    'full_name' => 'Store Manager',
+                    'role' => 'Manager',
+                ],
+                    [
+                        'username' => 'admin3',
+                        'full_name' => 'Admin User',
+                        'role' => 'Administrator',
+                    ],
+                    [
+                        'username' => 'manager3',
+                        'full_name' => 'Store Manager',
+                        'role' => 'Manager',
+                    ],
+            ],
+        ]);
+    }
 }
