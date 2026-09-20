1ea9e49196b13d95d20d8717a5c02d52cd7b8f7d
Abeleops <abelejohnjuarez03@gmail.com>
2026-09-21 00:55:27 +0800

    Fixed: Routes for home/about page

--- TFA1/app/Config/Routes.php
+++ TFA1/app/Config/Routes.php
@@ -6,5 +6,5 @@ use CodeIgniter\Router\RouteCollection;
 
 
 $routes->get('/', 'Home::index');
-$routes->get('home', 'Home::index');
+$routes->get('home(:hash)', 'Home::index');
 $routes->get('/about(:hash)', 'Page::about');
